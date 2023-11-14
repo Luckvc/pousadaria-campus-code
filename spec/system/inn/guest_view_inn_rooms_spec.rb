ea64a40fa417be3565ecad787d013cf050e86d58
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-14 14:53:56 -0300

    feat: guest view inn details from search

--- spec/system/inn/guest_view_inn_rooms_spec.rb
+++ spec/system/inn/guest_view_inn_rooms_spec.rb
@@ -18,9 +18,8 @@ describe 'Guest views inn rooms' do
                       price_cents:100_00, bathrooms:1, kitchen:false)
     inn.rooms.create!(number:'Leão', description:'Ótimo quarto com cozinha, uma cama de casal e uma de solteiro, tv, varanda com vista para a praia',
                       double_beds:1, single_beds:2, capacity:3, price_cents:150_00, bathrooms:0, kitchen:true)
-
     visit root_path
-    click_on 'Pousada Pousadona'
+    click_on 'Pousadona'
 
     expect(page).to have_content 'Quarto Elefante'
     expect(page).to have_content '2 Hóspedes'
@@ -28,9 +27,5 @@ describe 'Guest views inn rooms' do
     expect(page).to have_content '3 Hóspedes'
     expect(page).to have_content 'Descrição: Ótimo quarto com uma cama de casal, tv, varanda com vista para a praia'
     expect(page).to have_content 'Descrição: Ótimo quarto com cozinha, uma cama de casal e uma de solteiro, tv, varanda com vista para a praia'
-    
-  end
-  it 'from search' do
-    
   end
 end

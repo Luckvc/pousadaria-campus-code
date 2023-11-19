84a0d745a2531e49bc0d396a7e5a444c589985eb
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-19 13:32:47 -0300

    fix: inn index order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
  def index
   @last_inns = Inn.last(3)
-  @inns = Inn.all - @last_inns
+  @inns = Inn.all.order(:name) - @last_inns
  end
 end
---
+++ spec/system/reservation/guest_select_room_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'Guest selects room' do
+  it 'and sees booking page' do
+    
+  end
+end

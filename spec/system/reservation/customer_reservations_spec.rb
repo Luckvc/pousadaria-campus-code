b1682c6ba59b946d10d910fb7acb386d37215acd
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-20 02:18:30 -0300

    fix: naming and unused files

--- spec/system/reservation/customer_reservations_spec.rb
+++ spec/system/reservation/customer_reservations_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe 'Customer views his reservations' do
-  it 'from home page' do
+describe 'Customer reservations' do
+  it 'viewed from home page' do
     host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
     address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
                                city:'Campinas', state:'SP', cep:'15470-000')
     inn = host.create_inn!(name:'Pousadona', company_name:'Pousadona SN', cnpj:'456', phone:'223345',
-                            email:'pousadona@email.com', address:address2)
+                            email:'pousadona@email.com', address:address)
     room1 = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
                       double_beds:1, single_beds:0, capacity:2, 
                       price_cents:100_00, bathrooms:1, kitchen:false)
@@ -18,11 +18,13 @@ describe 'Customer views his reservations' do
     room2.reservations.create!(check_in_date: 5.months.from_now.to_date, check_out_date: 6.months.from_now.to_date,
                               guests:2, customer:customer)
     
+    login_as(customer, scope: :customer)
     visit root_path
     click_on 'Minhas Reservas'
 
     expect(page).to have_content 'Minhas Reservas'
-
-
+  end
+  it 'can be cancelled within 7 days' do
+    #TODO
   end
 end

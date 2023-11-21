d19497e41970df10aa914556e7fc9691211b2f2e
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-20 21:39:35 -0300

    wip: reservation status

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :room
   belongs_to :customer
   before_validation :generate_code
+  before_save :calculate_total
   validates :check_in_date, :check_out_date, :guests, :code, presence: true
 
   private
@@ -9,4 +10,28 @@ class Reservation < ApplicationRecord
   def generate_code
     self.code = SecureRandom.alphanumeric(8).upcase
   end
+
+  def calculate_total
+    if self.check_in_date.present? && self.check_out_date.present?
+      cd_matches = []
+      self.total = 0
+
+      self.room.custom_dates.each do |cd|
+        if range_overlap(self.check_in_date, self.check_out_date, cd.begin, cd.end)
+          cd_matches << cd
+        end
+      end
+
+      (self.check_in_date.to_date...self.check_out_date.to_date).each do |day|
+        price = self.room.price
+        cd_matches.each do |cd|
+          (cd.begin.to_date..cd.end.to_date).each do |cd_day|
+            price = cd.price if cd_day == day
+          end
+        end
+        self.total += price
+      end
+    end
+  end
+
 end
--- spec/system/reservation/customer_reservations_spec.rb
+++ spec/system/reservation/customer_reservations_spec.rb
@@ -24,8 +24,43 @@ describe 'Customer reservations' do
 
     expect(page).to have_content 'Minhas Reservas'
   end
-  it 'can be cancelled within 7 days' do
-    #TODO
+  it 'details' do
+    host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
+    address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
+                               city:'Campinas', state:'SP', cep:'15470-000')
+    inn = host.create_inn!(name:'Safari', company_name:'Pousada Safari SN', cnpj:'456', phone:'223345',
+                            email:'pousadona@email.com', address:address)
+    room = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
+                      double_beds:1, single_beds:0, capacity:2, 
+                      price:100.00, bathrooms:1, kitchen:false)
+    customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
+    allow(SecureRandom).to receive(:alphanumeric).and_return('ABC12345')
+    res = room.reservations.create!(check_in_date: Date.tomorrow, check_out_date: 5.days.from_now.to_date,
+                              guests:2, customer:customer)
+
+    login_as(customer, scope: :customer)
+    visit reservations_path
+    click_on 'ABC12345'
+
+    expect(page).to have_content 'Reserva ABC12345'
+    expect(page).to have_content 'Pousada: Safari'
+    expect(page).to have_content 'Quarto: Elefante'
+    expect(page).to have_content 'Status da Reserva: Confirmada'
+  end
+  it 'can be cancelled' do
+    host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
+    address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
+                               city:'Campinas', state:'SP', cep:'15470-000')
+    inn = host.create_inn!(name:'Pousadona', company_name:'Pousadona SN', cnpj:'456', phone:'223345',
+                            email:'pousadona@email.com', address:address)
+    room = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
+                      double_beds:1, single_beds:0, capacity:2, 
+                      price:100.00, bathrooms:1, kitchen:false)
+    customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
+    res = room.reservations.create!(check_in_date: Date.tomorrow, check_out_date: 5.days.from_now.to_date,
+                              guests:2, customer:customer)
+
+    visit reservation_path(res)
   end
   it 'have a 8 digit unique code' do
     #TODO

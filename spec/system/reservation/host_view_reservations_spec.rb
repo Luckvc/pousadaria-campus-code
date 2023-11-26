b8676ba1236d26de660739f380534ae37b422858
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-26 12:30:14 -0300

    fix: /git-old deleted, git problem

--- spec/system/reservation/host_view_reservations_spec.rb
+++ spec/system/reservation/host_view_reservations_spec.rb
@@ -36,4 +36,27 @@ describe 'Host views reservation' do
     expect(page).to have_content "Check-in: #{I18n.l(res_two.check_in_date.to_date)}"
     expect(page).to have_content "Check-in: #{I18n.l(res_three.check_in_date.to_date)}"
   end
+  it 'details' do
+    host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
+    address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
+      city:'Campinas', state:'SP', cep:'15470-000')
+    inn = host.create_inn!(name:'Safari', company_name:'Pousada Safari SN', cnpj:'456', phone:'223345',
+      email:'pousadona@email.com', address:address, pix:true)
+    room = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
+      double_beds:1, single_beds:0, capacity:2, price:100.00, bathrooms:1)
+    customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
+    res_one = room.reservations.create!(check_in_date: 2.days.from_now, check_out_date: 12.days.from_now,
+      guests:2, customer:customer)
+
+    login_as(host, scope: :user)
+    visit root_path
+    click_on 'Reservas'
+    click_on "Código: #{res_one.code}"
+
+    expect(page).to have_content "Reserva #{res_one.code}"
+    expect(page).to have_link "Quarto: #{res_one.room.number}"
+    expect(page).to have_content "Check-in: #{I18n.l(res_one.check_in_date.to_date)}"
+    expect(page).to have_content "Check-out: #{I18n.l(res_one.check_out_date.to_date)}"
+    expect(page).to have_content "Hóspedes: 2"
+  end
 end

6c81933cb7785064d678088e4694d520ece5e200
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-28 19:20:18 -0300

    fix: added travel_to on tests that would break depending on the time of day

--- spec/system/reservation/host_check_out_guest_spec.rb
+++ spec/system/reservation/host_check_out_guest_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include ActiveSupport::Testing::TimeHelpers
 
 describe 'Host checks-out guests' do
   it 'and sees check-out page' do
@@ -36,13 +37,16 @@ describe 'Host checks-out guests' do
       double_beds:1, single_beds:0, capacity:2, price:100.00, bathrooms:1)
     customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
     res = room.reservations.create!(check_in_date: 5.days.ago, check_out_date: Time.zone.now,
-      guests:2, customer:customer, checked_in_datetime: 5.days.ago, status:"ongoing")
+    guests:2, customer:customer, checked_in_datetime: 5.days.ago, status:"ongoing")
+    now = Time.zone.now
+    travel_to Time.zone.local(now.year, now.month, now.day, 10, 0, 00)
     
     login_as(host, scope: :user)
     visit root_path
     click_on 'Estadias Ativas'
     click_on "#{res.code}"
     click_on 'Check-out'
+    travel_back
 
     expect(page).to have_content "Check-out reserva #{res.code}"
     expect(page).to have_link "Quarto: #{res.room.number}"
@@ -51,7 +55,7 @@ describe 'Host checks-out guests' do
     expect(page).to have_content "Total a pagar: R$ 500,00"
     expect(page).to have_field "Forma de Pagamento"
   end
-  it 'and checks-out' do
+  it 'and checks-out on time' do
     host = User.create!(name: 'Lucas', email:'lucas@email.com', password:'password', host: true)
     host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
     address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
@@ -62,8 +66,10 @@ describe 'Host checks-out guests' do
       double_beds:1, single_beds:0, capacity:2, price:100.00, bathrooms:1)
     customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
     res = room.reservations.create!(check_in_date: 5.days.ago, check_out_date: Time.zone.now,
-      guests:2, customer:customer, checked_in_datetime: 5.days.ago, status:"ongoing")
-    
+    guests:2, customer:customer, checked_in_datetime: 5.days.ago, status:"ongoing")
+    now = Time.zone.now
+    travel_to Time.zone.local(now.year, now.month, now.day, 11, 0, 00)
+
     login_as(host, scope: :user)
     visit root_path
     click_on 'Estadias Ativas'
@@ -71,6 +77,7 @@ describe 'Host checks-out guests' do
     click_on 'Check-out'
     select "Cartão de Crédito", from: 'Forma de Pagamento'
     click_on 'Confirmar Check-out'
+    travel_back
 
     expect(page).to have_content "Reserva #{res.code}"
     expect(page).to have_link "Quarto: #{res.room.number}"
@@ -78,6 +85,39 @@ describe 'Host checks-out guests' do
     expect(page).to have_content "Check-in Registrado: #{res.checked_out_datetime}"
     expect(page).to have_content "Hóspedes: 2"
     expect(page).to have_content "Total: R$ 500,00"
+    expect(page).to have_content "Forma de Pagamento: Cartão de Crédito"
+    expect(page).to have_content "Status da Reserva: Finalizada"
+  end
+  it 'and checks-out late' do
+    host = User.create!(name: 'Lucas', email:'lucas@email.com', password:'password', host: true)
+    host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
+    address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
+      city:'Campinas', state:'SP', cep:'15470-000')
+    inn = host.create_inn!(name:'Safari', company_name:'Pousada Safari SN', cnpj:'456', phone:'223345',
+      email:'pousadona@email.com', address:address, pix:true, credit:true, debit:true, cash:true)
+    room = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
+      double_beds:1, single_beds:0, capacity:2, price:100.00, bathrooms:1)
+    customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
+    res = room.reservations.create!(check_in_date: 5.days.ago, check_out_date: Time.zone.now,
+    guests:2, customer:customer, checked_in_datetime: 5.days.ago, status:"ongoing")
+    now = Time.zone.now
+    travel_to Time.zone.local(now.year, now.month, now.day, 16, 0, 00)
+
+    login_as(host, scope: :user)
+    visit root_path
+    click_on 'Estadias Ativas'
+    click_on "#{res.code}"
+    click_on 'Check-out'
+    select "Cartão de Crédito", from: 'Forma de Pagamento'
+    click_on 'Confirmar Check-out'
+    travel_back
+
+    expect(page).to have_content "Reserva #{res.code}"
+    expect(page).to have_link "Quarto: #{res.room.number}"
+    expect(page).to have_content "Check-in Registrado: #{res.checked_in_datetime}"
+    expect(page).to have_content "Check-in Registrado: #{res.checked_out_datetime}"
+    expect(page).to have_content "Hóspedes: 2"
+    expect(page).to have_content "Total: R$ 600,00"
     expect(page).to have_content "Forma de Pagamento: Cartão de Crédito"
     expect(page).to have_content "Status da Reserva: Finalizada"
   end

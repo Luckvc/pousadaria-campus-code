ec1bbc9c95ea77ac159b6232865c1335efbb5bdd
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-28 17:24:55 -0300

    feat: inn show api has review score

--- app/controllers/api/v1/inns_controller.rb
+++ app/controllers/api/v1/inns_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::InnsController < ActionController::API
 
   def show
     begin 
-      render status:200, json: Inn.find(params[:id]).as_json(except: [:created_at, :updated_at, :cnpj, :company_name])
+      inn = Inn.find(params[:id]).as_json(except: [:created_at, :updated_at, :cnpj, :company_name]) 
+      score = Review.joins(reservation: :room).where("inn_id = ?", inn["id"]).average(:score) 
+      inn[:score] = score.to_s
+      render status:200, json: inn
     rescue
       return render status: :not_found
     end
--- spec/requests/api/inn_api_spec.rb
+++ spec/requests/api/inn_api_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 describe 'Inn API' do
   context 'GET /api/v1/inns/1' do
     it 'success' do
-      host = User.create!(name: 'Lucas', email:'test@email.com', password:'password', host: true)
-      address = Address.create!(street: 'Rua das ruas', number:'12', neighborhood:'centro',
-        city:'São Paulo', state:'SP', cep:'15470-000')
-      inn = host.create_inn!(name:'Safari', company_name:'Safari SN', cnpj:'123',
-        phone:'556618', email:'safari@email.com', address:address)
+      host = User.create!(name: 'João', email:'joao@email.com', password:'password', host: true)
+      address = Address.create!(street: 'Rua das torres', number:'28', neighborhood:'centro',
+        city:'Campinas', state:'SP', cep:'15470-000')
+      inn = host.create_inn!(name:'Safari', company_name:'Pousada Safari SN', cnpj:'456', phone:'223345',
+        email:'pousadona@email.com', address:address, pix:true, credit:true, debit:true, cash:true)
+      room = inn.rooms.create!(number:'Elefante', description:'Ótimo quarto com uma cama de casal', 
+        double_beds:1, single_beds:0, capacity:2, price:100.00, bathrooms:1)
+      customer = Customer.create!(name:'Roberto', cpf:'123', email:'roberto@email.com', password:'123456')
+      res = room.reservations.create!(check_in_date: 10.days.ago, check_out_date: 6.days.ago,
+        guests:2, customer:customer, checked_in_datetime: 10.days.ago, checked_out_datetime: 6.days.ago,
+        status:"completed")
+      res2 = room.reservations.create!(check_in_date: 5.days.ago, check_out_date: Time.zone.now,
+        guests:2, customer:customer, checked_in_datetime: 5.days.ago, checked_out_datetime: 1.days.ago,
+        status:"completed")
+      res3 = room.reservations.create!(check_in_date: 5.days.ago, check_out_date: Time.zone.now,
+        guests:2, customer:customer, checked_in_datetime: 5.days.ago, checked_out_datetime: 1.days.ago,
+        status:"completed")
+      res.create_review!(score: 5, message: 'Ótima estadia, adorei', customer:customer)
+      res2.create_review!(score: 3, message: 'Bom', customer:customer)
+      res3.create_review!(score: 4, message: 'Ótimo', customer:customer)
 
       get "/api/v1/inns/#{inn.id}"
 
@@ -15,7 +30,8 @@ describe 'Inn API' do
       expect(response.content_type).to include 'application/json'
       json_response = JSON.parse(response.body)
       expect(json_response["name"]).to eq 'Safari'
-      expect(json_response["email"]).to eq 'safari@email.com'
+      expect(json_response["email"]).to eq 'pousadona@email.com'
+      expect(json_response["score"]).to eq '4.0'
       expect(json_response.keys).not_to include("created_at")
       expect(json_response.keys).not_to include("updated_at")
       expect(json_response.keys).not_to include("cnpj")

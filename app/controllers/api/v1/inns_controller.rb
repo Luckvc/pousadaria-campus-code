8fed160ef94b6e776acce68252d6c54cb1b31897
Lucas Vasques <lucasvc1996@gmail.com>
2023-11-29 14:20:31 -0300

    feat(api): Improved full address

--- app/controllers/api/v1/inns_controller.rb
+++ app/controllers/api/v1/inns_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::InnsController < ActionController::API
       inn = Inn.find(params[:id]).as_json(except: [:created_at, :updated_at, :cnpj, :company_name]) 
       score = Review.joins(reservation: :room).where("inn_id = ?", inn["id"]).average(:score) 
       inn[:score] = score.to_s
+      inn = set_address(inn)
       render status:200, json: inn
     rescue
       return render status: :not_found
@@ -16,9 +17,7 @@ class Api::V1::InnsController < ActionController::API
     query = params[:query]
     inns = Inn.where("name LIKE ? AND active = ?", "%#{query}%", true)
               .as_json(except: [:created_at, :updated_at, :user_id, :cnpj, :company_name])
-    inns.each do |inn|
-      inn[:address] = Address.find(inn["address_id"]).as_json(except: [:created_at, :updated_at])
-    end
+    inns.each { |inn| inn = set_address(inn)}
     render status:200, json: inns
   end
 
@@ -28,5 +27,10 @@ class Api::V1::InnsController < ActionController::API
     response.headers['Access-Control-Allow-Origin'] = "http://127.0.0.1:3001"
   end
 
+  def set_address(inn)
+    inn[:address] = Address.find(inn["address_id"]).as_json(except: [:id, :created_at, :updated_at])
+    inn.delete("address_id")
+    inn
+  end
 
 end
